4dc37b8ada317d6d3053a0a6b0b3364aba90807f
Fremen DeRuvo <fremenderuvo@gmail.com>
2013-09-27 11:21:26 -0500

    invokes db with most colums

---
+++ db/migrate/20130927160609_add_properties_to_films.rb
@@ -0,0 +1,13 @@
+class AddPropertiesToFilms < ActiveRecord::Migration
+  def change
+    add_column :films,  :title,     :string 
+    add_column :films,  :director,  :string
+    add_column :films,  :poster,    :string
+    add_column :films,  :open,      :date  
+    add_column :films,  :synopsis,  :text
+    add_column :films,  :trailer,   :text
+    add_column :films,  :budget,    :integer
+    add_column :films,  :gross,   :integer
+  end
+
+end
